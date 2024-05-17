8d9096102e6c9fe5c4ed3dd2bb7ec1ecbe1ac870
AbdulHameed Mohamed <abdelhameed991999@gmail.com>
2024-05-17 09:24:33 +0300

    initial commit

--- ContentView.swift
+++ ContentView.swift
@@ -22,9 +22,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
---
+++ WeatherViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by AbdulHameed Mohamed on 17/05/2024.
+//
+
+import Foundation
